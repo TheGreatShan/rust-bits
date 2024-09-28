46c1acaa5e67cdab5f315aa33463703997dd3b61
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-09-28 14:25:31 +0200

    debug build created after running cargo build

---
+++ small-bits/target/debug/deps/small_bits-98eac0b980d59138.d
@@ -0,0 +1,5 @@
+/Users/shan15dev/work/rust-bits/small-bits/target/debug/deps/small_bits-98eac0b980d59138: src/main.rs
+
+/Users/shan15dev/work/rust-bits/small-bits/target/debug/deps/small_bits-98eac0b980d59138.d: src/main.rs
+
+src/main.rs:
