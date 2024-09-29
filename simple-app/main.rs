e938a2058b5ecff8b765a34bfe31824837ce2eae
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-09-29 09:00:12 +0200

    Refactored simple app to seperate directory

---
+++ simple-app/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    // ! indicated, that it is a Rust macro
+    // to start, use rustc main.rs -> This compiles an .exe for windows and some other sort of program for linux and macos
+    println!("Hello, world")
+}
