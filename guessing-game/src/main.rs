7cb992a0024de248c41612def1c5b1b80f8473d6
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-09-29 14:13:53 +0200

    game has endless loop

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -7,23 +7,26 @@ fn main() {
     println!("Guess a number and let's see if it matches with ours");
     
     // the number 1 and 100 are both also included
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop{
+        let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Input:");
+        println!("Input a number:");
+        
+        let mut guess = String::new();
     
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("MATEEEE thats not a number");
-
-    let guess :u32 = guess.trim().parse().expect("Parsing to integer failed");
-
-    println!("Your number was: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("You lost, your number was too small"),
-        Ordering::Greater => println!("You lost, your number was too big"),
-        Ordering::Equal => println!("YOU WIN!!!!")
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("MATEEEE thats not a number");
+    
+        let guess :u32 = guess.trim().parse().expect("Parsing to integer failed");
+    
+        println!("Your number was: {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("You lost, your number was too small"),
+            Ordering::Greater => println!("You lost, your number was too big"),
+            Ordering::Equal => println!("YOU WIN!!!!")
+        }
     }
+
 }
